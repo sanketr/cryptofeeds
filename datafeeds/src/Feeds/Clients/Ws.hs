366a2f906b724de1b9e4384f3e9d0d80dc59632c
Sanket Agrawal <sanket@laptop.local>
2018-02-28 20:40:05 -0500

    Removed swap files

--- datafeeds/src/Feeds/Clients/Ws.hs
+++ datafeeds/src/Feeds/Clients/Ws.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings,DeriveGeneric,ScopedTypeVariables #-}
 module Feeds.Clients.Ws
 (
  client
@@ -7,31 +7,85 @@ where
 
 import Wuss -- Websocket secure client - small wrapper around websockets library
 
-import Control.Concurrent (forkIO)
+import Control.Concurrent (MVar,newEmptyMVar,readMVar,putMVar,forkIO,threadDelay)
 import Control.Monad (forever, unless, void)
 import Data.Text (Text, pack)
-import Network.WebSockets (ClientApp, receiveData, sendClose, sendTextData,sendBinaryData)
-import Feeds.Gdax.Types (Heartbeat(..),GdaxRsp,RspMsg(..),ReqMsg(..),Request(..),Channels(..),ChannelMsg(..))
+import Network.WebSockets (ClientApp, Connection, receiveData, sendClose, sendTextData)
+import Feeds.Gdax.Types (Heartbeat(..),GdaxRsp,RspTyp(..),ReqTyp(..),Request(..),RequestMsg(..),Channels(..))
 import Data.Aeson.Text as A (encodeToLazyText)
+import Data.Text.Lazy.Encoding as LT (encodeUtf8)
+import Data.Aeson as A (decode)
+import qualified Data.Binary.Get as B
+import qualified Data.Store as B (Store,encode)
+import qualified Data.Text.Lazy as LT (Text)
+import Data.Maybe (maybe, fromJust)
+import qualified Data.Aeson.Types as A (FromJSON)
+import qualified Data.ByteString.Lazy as LBS (ByteString,hPut,fromStrict)
+import qualified Streaming.Prelude as S (Of, Stream, yield, mapM_,take)
+import Control.Monad.IO.Class (liftIO,MonadIO)
+import GHC.IO.Handle (Handle)
+import System.IO (openBinaryFile, IOMode(..), hFlush, hSetBuffering, BufferMode(..), hClose)
+import System.Exit (exitSuccess)
+
+-- | Imports for deriving Generic and Typeable
+import GHC.Generics
+import Data.Typeable
 
 -- This is a websocket client to connect to GDAX websocket feed
 client :: IO ()
 client = runSecureClient "ws-feed.gdax.com" 443 "/" ws
 
+-- Decode websocket json text - retain text if decoding failure else return decoded data
+msgDecode :: (A.FromJSON a, B.Store a) => LT.Text -> Either LT.Text a
+msgDecode inp = case A.decode . LT.encodeUtf8 $ inp of
+          Just val -> Right val
+          Nothing -> Left inp 
+
+-- Given a web socket connection, turn it into message stream - we will connect it to other streams like file append stream etc. to save down the data
+streamMsgsFromConn :: forall m. (MonadIO m, Monad m) => Connection -> S.Stream (S.Of (Either LT.Text LBS.ByteString)) m ()
+streamMsgsFromConn conn = forever $ do
+                -- Block waiting for the message
+                msg <- liftIO $ receiveData conn
+                case (msgDecode msg :: Either LT.Text GdaxRsp) of
+                  Left blob -> (S.yield :: a -> S.Stream (S.Of a) m () ) . Left $ blob
+                  Right res -> (S.yield :: a -> S.Stream (S.Of a) m () ) . Right . LBS.fromStrict . B.encode $ res  -- explicit type signature for S.yield because the compiler can't deduce it is the same monad m from type signature - use forall to enforce scoped types
+
+data LogType = Normal | Error deriving (Show,Generic,Typeable)
+
+getFileHandle :: LogType -> IO Handle
+getFileHandle logtyp = case logtyp of
+                Normal -> openBinaryFile "normal.log" AppendMode
+                Error -> openBinaryFile "unknown.log" AppendMode
+                  
+
+logDataToFile :: Connection -> Handle -> Handle -> IO()
+logDataToFile conn hlog hlogerr = do
+                  -- msg is of type: Stream (Of (Either Text BS) m ().
+                  -- Append to log if no error. Else log to logerr handle
+                  S.mapM_ (either (\x -> LBS.hPut hlogerr (LT.encodeUtf8 x)) (\x -> LBS.hPut hlog x)) $ S.take 10 (streamMsgsFromConn conn)
+
 ws :: ClientApp ()
 ws connection = do
   putStrLn "Connected!"
+  dieSignal <- newEmptyMVar :: IO (MVar String)
+  hlog <- getFileHandle Normal -- file handle for normal logging
+  hlogerr <- getFileHandle Error -- file handle for error logging
+  mapM_ (`hSetBuffering` NoBuffering) [hlog,hlogerr] -- flush to disk immediately
 
-  void . forkIO . forever $ do
-    message <- receiveData connection
-    print (message :: Text)
+  forkIO $ do
+    logDataToFile connection hlog hlogerr
+    putMVar dieSignal "Done with processing first 10 messages - test mode"
 
   -- Let us build a JSON request for heartbeat to ETH-EUR instrument
-  let req1 = A.encodeToLazyText (Request {_req_type = Subscribe, _req_channels = [Channels (ChannelMsg {_channel_name = HeartbeatMsg, _channel_product_ids = ["ETH-EUR"]})]})
-  -- Send heartbeat subscription message -
+  let req1 = A.encodeToLazyText Request {_req_type = Subscribe, _req_channels = RequestMsg $ map (\(reqtyp,prdids) -> Channels {_channel_name = reqtyp, _channel_product_ids = prdids}) [(HeartbeatTyp,["ETH-EUR"]),(Level2Typ,["ETH-EUR"])]}
+  -- Send heartbeat subscription message
   sendTextData connection req1
 
-  -- let us just loop forever on input from REPL so we never exit
-  forever $ getLine
-  -- Will never get here, but yeah, let us just keep this placeholder for now - will replace with some kind of clean shutdown mechanism later
+  -- Don't do any resource cleanup before mvar otherwise we will free resources while they are in use!
+  -- let us wait for procMsg to exit
+  dieMsg <- readMVar dieSignal
+  -- Will replace with some kind of clean shutdown mechanism later
   sendClose connection (pack "Bye!")
+  mapM_ hClose [hlog,hlogerr] -- Close all file handles when done -- To do - resourceT version instead
+  print dieMsg
+  exitSuccess
--- datafeeds/src/Feeds/Gdax/Types.hs
+++ datafeeds/src/Feeds/Gdax/Types.hs
@@ -1,12 +1,12 @@
-{-# LANGUAGE OverloadedStrings,ScopedTypeVariables,DeriveGeneric, TemplateHaskell #-}
+{-# LANGUAGE OverloadedStrings,ScopedTypeVariables,DeriveGeneric,TemplateHaskell #-}
 
 module Feeds.Gdax.Types
 (
  Channels(..),
- ReqMsg(..),
- RspMsg(..),
- ChannelMsg(..),
+ ReqTyp(..),
+ RspTyp(..),
  Request(..),
+ RequestMsg(..),
  Heartbeat(..),
  Ticker(..),
  Snapshot(..),
@@ -17,6 +17,7 @@ module Feeds.Gdax.Types
 where
 
 import Data.Aeson.TH
+import Data.Store
 import GHC.Generics
 import Data.Typeable
 import Data.Text as T (Text,unpack)
@@ -26,33 +27,43 @@ import Data.Vector as V (Vector)
 import Data.ByteString.Lazy as LBS (ByteString)
 import Control.Exception (Exception)
 
-data ReqMsg = Subscribe | Unsubscribe deriving (Show, Generic,Typeable)
-deriveJSON defaultOptions{constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''ReqMsg
+data ReqTyp = Subscribe | Unsubscribe deriving (Show, Generic,Typeable)
+deriveJSON defaultOptions{constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''ReqTyp
+instance Store ReqTyp
 
 -- Response types - heartbeat, ticker, snapshot, l2update
-data RspMsg = HeartbeatMsg | TickerMsg | SnapshotMsg | L2updateMsg deriving (Show, Generic,Typeable)
-deriveJSON defaultOptions{constructorTagModifier = (\x -> Prelude.take (-3 + (Prelude.length x)) (Prelude.map toLower x)),omitNothingFields = True} ''RspMsg
+data RspTyp = HeartbeatTyp | TickerTyp | SnapshotTyp | Level2Typ | L2UpdateTyp deriving (Show, Generic,Typeable)
+deriveJSON defaultOptions{constructorTagModifier = (\x -> Prelude.take (-3 + (Prelude.length x)) (Prelude.map toLower x)),omitNothingFields = True} ''RspTyp
+instance Store RspTyp
 
-data ChannelMsg = ChannelMsg { _channel_name :: RspMsg, _channel_product_ids :: [T.Text]} deriving (Show,Generic,Typeable)
-deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 9,constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''ChannelMsg
+data Channels = Channels { _channel_name :: RspTyp, _channel_product_ids :: [T.Text]} deriving (Show,Generic,Typeable)
+deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 9,constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''Channels
+instance Store Channels
 
-data Channels = Channels ChannelMsg deriving (Show,Generic,Typeable)
-deriveJSON defaultOptions{constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''Channels
+data RequestMsg = RequestMsg [Channels] deriving (Show, Generic,Typeable)
+deriveJSON defaultOptions{constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''RequestMsg
+instance Store RequestMsg
 
-data Request = Request { _req_type :: ReqMsg, _req_channels :: [Channels] } deriving (Show, Generic,Typeable)
+data Request = Request { _req_type :: ReqTyp, _req_channels :: RequestMsg } deriving (Show, Generic,Typeable)
 deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 5,constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''Request
+instance Store Request
 
-data Heartbeat = Heartbeat { _hb_type :: RspMsg, _hb_sequence:: Int64, _hb_last_trade_id :: Int64, _hb_product_id :: T.Text, _hb_time :: T.Text } deriving (Show, Generic,Typeable)
+data Heartbeat = Heartbeat { _hb_type :: RspTyp, _hb_sequence:: Int64, _hb_last_trade_id :: Int64, _hb_product_id :: T.Text, _hb_time :: T.Text } deriving (Show, Generic,Typeable)
 deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 4,constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''Heartbeat 
+instance Store Heartbeat
 
-data Ticker = Ticker { _tick_type :: RspMsg, _tick_product_id :: T.Text, _tick_price :: T.Text, _tick_open_24h :: T.Text, _tick_volume_24h :: T.Text, _tick_low_24h :: T.Text, _tick_high_24h :: T.Text, _tick_volume_30d :: T.Text, _tick_best_bid :: T.Text, _tick_best_ask :: T.Text, _tick_side :: T.Text, _tick_time :: T.Text, _tick_trade_id :: Int64, _tick_last_size :: T.Text } deriving (Show, Generic,Typeable)
+data Ticker = Ticker { _tick_type :: RspTyp, _tick_product_id :: T.Text, _tick_price :: T.Text, _tick_open_24h :: T.Text, _tick_volume_24h :: T.Text, _tick_low_24h :: T.Text, _tick_high_24h :: T.Text, _tick_volume_30d :: T.Text, _tick_best_bid :: T.Text, _tick_best_ask :: T.Text, _tick_side :: T.Text, _tick_time :: T.Text, _tick_trade_id :: Int64, _tick_last_size :: T.Text } deriving (Show, Generic,Typeable)
 deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 6,constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''Ticker
+instance Store Ticker
 
-data Snapshot = Snapshot { _snp_type :: RspMsg, _snp_product_id :: T.Text, _snp_bids :: [(T.Text,T.Text)], _snp_asks :: [(T.Text,T.Text)]} deriving (Show, Generic,Typeable)
+data Snapshot = Snapshot { _snp_type :: RspTyp, _snp_product_id :: T.Text, _snp_bids :: [(T.Text,T.Text)], _snp_asks :: [(T.Text,T.Text)]} deriving (Show, Generic,Typeable)
 deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 5,constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''Snapshot 
+instance Store Snapshot
 
-data L2Update = L2Update { _l2upd_type :: RspMsg ,_l2upd_product_id :: T.Text, _l2upd_changes :: [(T.Text,T.Text,T.Text)]} deriving (Show, Generic,Typeable)
+data L2Update = L2Update { _l2upd_type :: RspTyp ,_l2upd_product_id :: T.Text, _l2upd_changes :: [(T.Text,T.Text,T.Text)]} deriving (Show, Generic,Typeable)
 deriveJSON defaultOptions{fieldLabelModifier = Prelude.drop 7,constructorTagModifier = Prelude.map toLower,omitNothingFields = True} ''L2Update
+instance Store L2Update
 
 data GdaxRsp = GdRHb Heartbeat | GdRTick Ticker | GdRSnap Snapshot | GdRL2Up L2Update deriving (Show, Generic,Typeable)
 deriveJSON defaultOptions{omitNothingFields = True, sumEncoding  = UntaggedValue} ''GdaxRsp
+instance Store GdaxRsp
