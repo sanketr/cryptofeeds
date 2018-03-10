c812696d7a42affcba3c87e6a85274b2f15f5d10
Sanket <none>
2018-03-09 23:14:53 -0500

    Initial prototyping of resilient data capture

--- datafeeds/src/Feeds/Clients/Utils.hs
+++ datafeeds/src/Feeds/Clients/Utils.hs
@@ -1,14 +1,14 @@
 {-# LANGUAGE OverloadedStrings,ScopedTypeVariables #-}
 module Feeds.Clients.Utils
 (
- switchFiles,
+ logSwitch
 )
 where
 
-import Control.Concurrent (MVar,newMVar,isEmptyMVar,readMVar,putMVar,tryPutMVar,takeMVar,tryTakeMVar,threadDelay,forkFinally)
-import Control.Concurrent.Async (async,waitCatch)
-import Control.Exception.Safe (bracket,bracketOnError,try)
-import System.Directory (createDirectoryIfMissing,listDirectory)
+import Control.Concurrent (MVar,newMVar,putMVar,tryPutMVar,takeMVar,tryTakeMVar,threadDelay,forkFinally)
+import Control.Monad (unless)
+import Control.Exception.Safe (bracket,bracketOnError)
+import System.Directory (createDirectoryIfMissing,doesFileExist)
 import System.FilePath (takeDirectory,(</>))
 import System.IO (openBinaryFile,IOMode(..),hSetBuffering, BufferMode(..),hClose)
 import GHC.IO.Handle (Handle)
@@ -24,7 +24,7 @@ b) any of the log handles couldn't be used
 
 Connection ->
 
-acquire file handle1 with bracket - put in mvar - on cleanup, Left Exception in mvar - users should exit on error
+acquire file handle1 with bracket - put in mvar
 acquire file handle2 with bracket - put in mvar 
 
 if any async exception, cancel the thread that uses those handles, throw exception
@@ -42,12 +42,19 @@ openFile fpath mode = do
 -- a function that switches from one filepath to another
 -- Takes handle to old file path, closes it, and returns handle to new file path
 -- Can throw async exception
-switchFiles :: Maybe Handle -> FilePath -> IOMode -> IO Handle
-switchFiles ohdl npath mode = do
+switchFiles :: Maybe Handle -> IOMode -> Int -> (Int -> FilePath) -> IO Handle
+switchFiles ohdl mode ctr getPath = do
   case ohdl of
     Just hdl -> hClose hdl
     Nothing -> return () -- first time when we use it, there is no old handle
-  openFile npath mode -- return handle to new file
+  -- First check if the file with number ctr already exists - if it does, keep incrementing the counter until
+  -- we find a filename that hasn't been created before. This lets us handle restart from multiple crashes by
+  -- creating a new log instead of accidentally overwriting an existing log
+  let loop num = do
+            exists <- doesFileExist $ getPath num
+            -- Warning - might overflow since there is no upper bound - partial function
+            if exists then loop (num + 1) else openFile (getPath num) mode
+  loop ctr -- return handle to new file
 
 -- Get date as integer as well as string - we use string to create directories etc. and integer to keep track of date rollover
 getDate :: IO (Integer,String)
@@ -62,26 +69,19 @@ getDate = do
 data LogState = LogState {logdt:: Integer, logctr:: Int, logstart :: Bool } deriving (Show)
 data LogType = Normal | Error deriving (Show)
 
-getLogCtr :: FilePath -> IO (Maybe Int)
-getLogCtr fpath = do
-  r <- waitCatch =<< (async $ listDirectory fpath)
-  case r of
-    Left _ -> return Nothing
-    Right files -> undefined -- Todo - look for file pattern for log typ, and get max number if exists. Else Nothing
-
 -- We will execute this one in a separate thread and continously monitor for date change
 -- Can throw exception while MVar is already blocked by takeMVar
 logSwitchH :: Int -> FilePath -> LogState -> MVar (Maybe Handle,Maybe Handle) -> IO ()
 logSwitchH interval logbasedir st mvar = do
   let loop state = do
-        threadDelay interval -- Wait for specified interval and then check below if date changed
+        unless (logstart state) $ threadDelay interval -- Dont delay first iteration
         (ndt,ndtstr) <- getDate
         case (logdt state < ndt) || logstart state of
-          True -> do -- Either date incremented or first time
+          True -> do -- Either date rolled over to next day or first time
                 let newctr = if logdt state < ndt then 1 else 1 + logctr state -- In case of new date, start with 1
                 bracketOnError (takeMVar mvar) (tryPutMVar mvar) $ \(h1,h2) ->
-                  bracket (switchFiles h1 (getLogPath Normal ndtstr newctr) WriteMode) hClose $ \hdl1 ->
-                    bracket (switchFiles h2 (getLogPath Error ndtstr newctr) WriteMode) hClose $ \hdl2 ->
+                  bracketOnError (switchFiles h1 WriteMode newctr (getLogPath Normal ndtstr)) hClose $ \hdl1 ->
+                    bracketOnError (switchFiles h2 WriteMode newctr (getLogPath Error ndtstr)) hClose $ \hdl2 ->
                       putMVar mvar (Just hdl1,Just hdl2)
                 -- TODO - make sure numbering is correct
                 loop LogState { logdt = ndt, logctr = newctr, logstart = False }
@@ -89,18 +89,26 @@ logSwitchH interval logbasedir st mvar = do
         where
           getLogPath :: LogType -> String -> Int -> FilePath
           getLogPath typ dt num = Data.List.foldl' (</>) "" [logbasedir,dt,map toLower (show typ) ++ show num ++ ".log"]
-
-  loop st -- loop forever until killed - make sure to do forkFinally for handle cleanup - use tryTakeMVar to avoid blocking
+  loop st -- loop forever until killed - make sure to do forkFinally for handle cleanup 
 
 -- This function takes care of creating log file handles, and closing them - async exceptions are handled
-logSwitch :: Int -> FilePath -> IO (MVar (Maybe Handle,Maybe Handle))
-logSwitch interval logbasedir = do
+logSwitch :: Int -> FilePath -> MVar String -> IO (MVar (Maybe Handle,Maybe Handle))
+logSwitch interval logbasedir dieSignal = do
   (dt,dtstr) <- getDate
   mvar <- newMVar (Nothing,Nothing)
   let initst = LogState { logdt = dt, logctr = 0, logstart = True}
       cleanUp = do
               hdls <- tryTakeMVar mvar -- mvar may not be filled in case of exception - so, don't block
               maybe (return ()) (\(h1,h2) -> mapM_ (maybe (return ()) hClose) [h1,h2]) hdls
-              tryPutMVar mvar (Nothing,Nothing) >> return () -- Make sure to put empty values in MVar
-  forkFinally (logSwitchH interval logbasedir initst mvar) (\_ -> cleanUp)
+              print "Cleaning up"
+              tryPutMVar mvar (Nothing,Nothing) >> putMVar dieSignal "Exception during log switch logic" -- Make sure to put empty values in MVar
+  forkFinally (logSwitchH interval logbasedir initst mvar) (const cleanUp)
   return mvar
+
+{--
+main = do
+  dieSignal <- newEmptyMVar
+  mvarLogHdls <- logSwitch 60000000 "test/test/abc" dieSignal
+  print "acquired mvar"
+  takeMVar dieSignal
+--}
--- datafeeds/src/Feeds/Clients/Ws.hs
+++ datafeeds/src/Feeds/Clients/Ws.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings,DeriveGeneric,ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings,ScopedTypeVariables #-}
 module Feeds.Clients.Ws
 (
  client
@@ -7,11 +7,11 @@ where
 
 import Wuss -- Websocket secure client - small wrapper around websockets library
 
-import Control.Concurrent (MVar,newEmptyMVar,readMVar,putMVar,forkIO)
+import Control.Concurrent (MVar,newEmptyMVar,readMVar,putMVar,forkFinally,threadDelay)
 import Control.Monad (forever)
+import Control.Exception(Exception,throw)
 import Data.Text (pack)
 import Network.WebSockets (ClientApp, Connection, receiveData, sendClose, sendTextData)
-import Feeds.Gdax.Types (GdaxRsp,RspTyp(..),ReqTyp(..),Request(..),RequestMsg(..),Channels(..))
 import Data.Aeson.Text as A (encodeToLazyText)
 import Data.Text.Lazy.Encoding as LT (encodeUtf8)
 import Data.Aeson as A (decode)
@@ -22,12 +22,10 @@ import qualified Data.ByteString.Lazy as LBS (ByteString,hPut,fromStrict)
 import qualified Streaming.Prelude as S (Of, Stream, yield, mapM_,take)
 import Control.Monad.IO.Class (liftIO,MonadIO)
 import GHC.IO.Handle (Handle)
-import System.IO (openBinaryFile, IOMode(..),hSetBuffering, BufferMode(..), hClose)
 import System.Exit (exitSuccess)
 
--- | Imports for deriving Generic and Typeable
-import GHC.Generics
-import Data.Typeable
+import Feeds.Gdax.Types (GdaxRsp,RspTyp(..),ReqTyp(..),Request(..),RequestMsg(..),Channels(..))
+import Feeds.Clients.Utils (logSwitch)
 
 -- This is a websocket client to connect to GDAX websocket feed
 client :: IO ()
@@ -48,28 +46,39 @@ streamMsgsFromConn conn = forever $ do
                   Left blob -> (S.yield :: a -> S.Stream (S.Of a) m () ) . Left $ blob
                   Right res -> (S.yield :: a -> S.Stream (S.Of a) m () ) . Right . LBS.fromStrict . B.encode $ res  -- explicit type signature for S.yield because the compiler can't deduce it is the same monad m from type signature - use forall to enforce scoped types
 
-data LogType = Normal | Error deriving (Show,Generic,Typeable)
+data NoLogFileException = NormalLogException String | ErrorLogException String
 
-getFileHandle :: LogType -> IO Handle
-getFileHandle logtyp = case logtyp of
-                Normal -> openBinaryFile "normal.log" AppendMode
-                Error -> openBinaryFile "unknown.log" AppendMode
+instance Show NoLogFileException where
+  show (NormalLogException e) = "NormalLogException: There was an issue when accessing normal log file. " ++ e
+  show (ErrorLogException e) = "ErrorLogException: There was an issue when accessing normal log file. " ++ e
 
-logDataToFile :: Connection -> Handle -> Handle -> IO()
-logDataToFile conn hlog hlogerr =
-                  S.mapM_ (either (LBS.hPut hlogerr . LT.encodeUtf8) (LBS.hPut hlog)) $ S.take 10 (streamMsgsFromConn conn)
+instance Exception NoLogFileException
+
+logDataToFile :: Connection -> MVar (Maybe Handle,Maybe Handle) -> IO()
+logDataToFile conn mvarhdls = S.mapM_ (either logToError logToNormal) $ S.take 10 (streamMsgsFromConn conn)
+            where
+              -- We get current log handle for respective log, and write to it - if for any reasons, no log handle
+              -- is found, an exception is thrown which will kill the main thread. We can add logic to restart the
+              -- the whole data capture process
+              logToNormal :: LBS.ByteString -> IO()
+              logToNormal msg = do
+                (hlog,_) <- readMVar mvarhdls
+                maybe (throw $ ErrorLogException "Normal log file couldnt be opened") (`LBS.hPut` msg) hlog
+              
+              logToError :: LT.Text -> IO()
+              logToError msg = do
+                (_,hlogerr) <- readMVar mvarhdls
+                maybe (throw $ ErrorLogException "Error log file couldnt be opened") (\x -> LBS.hPut x $ LT.encodeUtf8 msg) hlogerr 
+              
 
 ws :: ClientApp ()
 ws connection = do
   putStrLn "Connected!"
   dieSignal <- newEmptyMVar :: IO (MVar String)
-  hlog <- getFileHandle Normal -- file handle for normal logging
-  hlogerr <- getFileHandle Error -- file handle for error logging
-  mapM_ (`hSetBuffering` NoBuffering) [hlog,hlogerr] -- flush to disk immediately
 
-  forkIO $ do
-    logDataToFile connection hlog hlogerr
-    putMVar dieSignal "Done with processing first 10 messages - test mode"
+  mvarLogHdls <- logSwitch 60000000 "test/gdax" dieSignal
+  threadDelay 1000000 -- Delay for one second to allow for logs to be created in above background thread
+  forkFinally (logDataToFile connection mvarLogHdls) (either (putMVar dieSignal . show) (\_ -> putMVar dieSignal "Done with processing messages - test mode"))
 
   -- Let us build a JSON request for heartbeat to ETH-EUR instrument
   let req1 = A.encodeToLazyText Request {_req_type = Subscribe, _req_channels = RequestMsg $ map (\(reqtyp,prdids) -> Channels {_channel_name = reqtyp, _channel_product_ids = prdids}) [(HeartbeatTyp,["ETH-EUR"]),(Level2Typ,["ETH-EUR"]),(TickerTyp,["ETH-EUR"])]}
@@ -81,6 +90,5 @@ ws connection = do
   dieMsg <- readMVar dieSignal
   -- Will replace with some kind of clean shutdown mechanism later
   sendClose connection (pack "Bye!")
-  mapM_ hClose [hlog,hlogerr] -- Close all file handles when done -- To do - resourceT version instead
   print dieMsg
   exitSuccess
