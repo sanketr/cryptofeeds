8b455eb98dc7230098cb390f3edc8ecfe7273d6d
Sanket <none>
2018-03-11 22:32:09 -0400

    Revert hstack script commit

--- datafeeds/buildAndrun.sh
+++ datafeeds/buildAndrun.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # First build the project using haskell-stack
-stack build
+if [ -x "$(command -v hstack)" ]; then
+	hstack build
+else
+	stack build
+fi
+
 
 # Find the location of binary and execute it - it will start printing heartbeat messages for now for one of the instruments
 exeloc=`find ./ -name datafeeds-exe|grep bin`
